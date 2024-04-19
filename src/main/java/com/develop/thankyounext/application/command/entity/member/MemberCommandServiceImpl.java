9f7d847faacdb7b390f1ba3473cfae1bf9e8db49
Luke_YS <yjsmk0902@gmail.com>
2024-04-20 00:01:51 +0900

    :sparkles: MemberCommandService 수정 (#38)
    - signup, logout 로직 추가

--- src/main/java/com/develop/thankyounext/application/command/entity/member/MemberCommandServiceImpl.java
+++ src/main/java/com/develop/thankyounext/application/command/entity/member/MemberCommandServiceImpl.java
@@ -2,15 +2,25 @@ package com.develop.thankyounext.application.command.entity.member;
 
 import com.develop.thankyounext.domain.dto.base.common.AuthenticationDto;
 import com.develop.thankyounext.domain.dto.member.MemberRequest.UpdateMember;
+import com.develop.thankyounext.domain.dto.result.ResultResponse.AuthResult;
 import com.develop.thankyounext.domain.dto.result.ResultResponse.MemberResult;
 import com.develop.thankyounext.domain.entity.Member;
+import com.develop.thankyounext.domain.enums.UserRoleEnum;
 import com.develop.thankyounext.domain.repository.member.MemberRepository;
 import com.develop.thankyounext.global.exception.handler.MemberHandler;
 import com.develop.thankyounext.global.payload.code.status.ErrorStatus;
+import com.develop.thankyounext.infrastructure.config.redis.driver.RedisDriver;
+import com.develop.thankyounext.infrastructure.config.security.jwt.driver.JwtDriver;
 import com.develop.thankyounext.infrastructure.converter.MemberConverter;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.Optional;
+import java.util.function.Consumer;
+
+import static com.develop.thankyounext.domain.dto.auth.AuthRequest.SignUp;
 
 @Service
 @Transactional
@@ -18,7 +28,9 @@ import org.springframework.transaction.annotation.Transactional;
 public class MemberCommandServiceImpl implements MemberCommandService{
 
     private final MemberRepository memberRepository;
-
+    private final PasswordEncoder passwordEncoder;
+    private final JwtDriver jwtDriver;
+    private final RedisDriver redisDriver;
     private final MemberConverter memberConverter;
 
     @Override
@@ -35,28 +47,53 @@ public class MemberCommandServiceImpl implements MemberCommandService{
         return memberConverter.toMemberResult(currentMember);
     }
 
+    @Override
+    public AuthResult signup(SignUp request) {
+        validateSignUpRequest(request);
+
+        Member newMember = memberConverter.toMember(request);
+        newMember.passwordEncode(passwordEncoder);
+        newMember.updateRole(UserRoleEnum.USER);
+        memberRepository.save(newMember);
+
+        return memberConverter.toAuthResult(newMember);
+    }
+
+    @Override
+    public void logout(String accessToken, Long memberId) {
+        Long expiration = jwtDriver.getExpiration(accessToken);
+        redisDriver.expireAccessToken(accessToken, expiration);
+        memberRepository.findById(memberId)
+                .ifPresent(member -> member.updateRefreshToken(null));
+    }
+
     private static void updateMember(UpdateMember request, Member currentMember) {
+        validateAndUpdatePassword(request, currentMember);
+        updateIfPresent(request.name(), currentMember::updateName);
+        updateIfPresent(request.description(), currentMember::updateDescription);
+        updateIfPresent(request.studentId(), currentMember::updateStudentId);
+        updateIfPresent(request.linkUrlList(), currentMember::updateLinkUrlList);
+    }
+
+    private static void validateAndUpdatePassword(UpdateMember request, Member currentMember) {
         if (request.password() != null) {
-            if (request.password().equals(request.passwordCheck())) {
-                currentMember.updatePassword(request.password());
+            if (!request.password().equals(request.passwordCheck())) {
+                throw new MemberHandler(ErrorStatus.MEMBER_UPDATE_PASSWORD_NOT_EQUAL_BAD_REQUEST);
             }
-            throw new MemberHandler(ErrorStatus.MEMBER_UPDATE_PASSWORD_NOT_EQUAL_BAD_REQUEST);
+            currentMember.updatePassword(request.password());
         }
+    }
 
-        if (request.name() != null) {
-            currentMember.updateName(request.name());
+    private static <T> void updateIfPresent(T value, Consumer<T> updateFunction) {
+        Optional.ofNullable(value).ifPresent(updateFunction);
+    }
+
+    private void validateSignUpRequest(SignUp request) {
+        if (Boolean.TRUE.equals(memberRepository.existsByEmail(request.email()))) {
+            throw new MemberHandler(ErrorStatus.MEMBER_EMAIL_EXISTED);
         }
-
-        if (request.description() != null) {
-            currentMember.updateDescription(request.description());
-        }
-
-        if (request.studentId() != null) {
-            currentMember.updateStudentId(request.studentId());
-        }
-
-        if (request.linkUrlList() != null) {
-            currentMember.updateLinkUrlList(request.linkUrlList());
+        if (Boolean.TRUE.equals(memberRepository.existsByStudentId(request.studentId()))) {
+            throw new MemberHandler(ErrorStatus.MEMBER_STUDENT_ID_EXISTED);
         }
     }
 }
