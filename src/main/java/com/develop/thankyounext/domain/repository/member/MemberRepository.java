85b7856d9957bf54e3a6a69e74ce7644508b33b2
Luke_YS <yjsmk0902@gmail.com>
2024-04-20 00:05:45 +0900

    :sparkles: MemberRepository 수정 (#38)
    Data JPA 추가
    - findByRefreshToken
    - findByEmail
    - existsByEmail
    - existsByStudentId

--- src/main/java/com/develop/thankyounext/domain/repository/member/MemberRepository.java
+++ src/main/java/com/develop/thankyounext/domain/repository/member/MemberRepository.java
@@ -3,5 +3,14 @@ package com.develop.thankyounext.domain.repository.member;
 import com.develop.thankyounext.domain.entity.Member;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface MemberRepository extends JpaRepository<Member, Long>, MemberQueryDSL {
+     Optional<Member> findByRefreshToken(String refreshToken);
+
+     Optional<Member> findByEmail(String email);
+
+     Boolean existsByEmail(String email);
+
+     Boolean existsByStudentId(String string);
 }
